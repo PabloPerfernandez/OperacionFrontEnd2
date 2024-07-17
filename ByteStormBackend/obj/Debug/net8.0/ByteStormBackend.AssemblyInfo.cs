f6795fb994d17290e648d1dfcfe9f82f599299b6
PabloPerfernandez <132550960+PabloPerfernandez@users.noreply.github.com>
2024-07-17 10:33:02 +0200

    17/07

--- ByteStormBackend/obj/Debug/net8.0/ByteStormBackend.AssemblyInfo.cs
+++ ByteStormBackend/obj/Debug/net8.0/ByteStormBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ByteStormBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0f3b3e53a1e6811ee502c330709fe8d79cec02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5edc39eca3e9b88140bb42b3841f9689641331")]
 [assembly: System.Reflection.AssemblyProductAttribute("ByteStormBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ByteStormBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
